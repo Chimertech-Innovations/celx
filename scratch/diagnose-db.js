4bb58f7653a8ad1dc11e680e325c50a63c061932
silambarasan <csesilambu3@gmail.com>
2026-06-29 21:41:44 +0530

    databas

--- scratch/diagnose-db.js
+++ scratch/diagnose-db.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
